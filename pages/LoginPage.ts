cf268c0cab7e0d5864e37483603e425dfb63094b
chandima <chandimananayakkara94@gmail.com>
2026-06-10 16:03:57 +0530

    feat: add Login page object model

---
+++ pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import type { Page } from "@playwright/test";
+
+export class LoginPage{
+    private page:Page
+
+    constructor(page:Page){
+        this.page = page
+    }
+
+    async goto(){
+        await this.page.goto('https://www.saucedemo.com/')
+    }
+
+    async login(username: string, password: string){
+        await this.page.getByPlaceholder('Username').fill(username)
+        await this.page.getByPlaceholder('Password').fill(password)
+        await this.page.getByRole('button', {name:'Login'}).click()
+
+    }
+
+    async isLoggedIn(){
+        return await this.page.getByText('Products').isVisible()
+    }
+
+    async getErrorMessage(){
+        return await this.page.getByText('Error:').textContent()
+    }
+}
